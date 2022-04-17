abfe61d3459f0b7184117264e2bf30dae5561f87
imranahmed1990 <imranmarin1990@gmail.com>
2022-04-18 03:23:44 +0600

    another finishing commit

--- src/Page/BookingFood/BookingFood.js
+++ src/Page/BookingFood/BookingFood.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './BookingFood.css';
 
 const BookingFood = () => {
     return (
-        <div className='text-center mt-5 '>
+        <div className='text-center mt-5 booking'>
             <h4>Please Enter Your Name</h4>
             <div className='text-center'>
                 <input className='my-2' type="text" name="name" id="" placeholder='Enter Your Name' />
--- src/Page/OrderPage/OrderPage.js
+++ src/Page/OrderPage/OrderPage.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Link, useParams } from 'react-router-dom';
+import './OrderPage.css';
 
 const OrderPage = (props) => {
 
     const { serviceId, name } = useParams();
     return (
-        <div>
+        <div className=' order text-center mt-5'>
             <h4>Welcome to Order Page Are you want to order Please Click Order Confirm button</h4>
             <h2>{serviceId}</h2>
             <p>{serviceId.name}</p>
 
-            <Link to='/bookingfood'>Booking Food</Link>
+            <Link to='/bookingfood'><button className='btn btn-warning '>Place Order</button></Link>
 
 
         </div>
